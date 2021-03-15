f547d551c1fa21f01cafc446dd6ff575a048c028
graysonfattaleh <71087950+graysonfattaleh@users.noreply.github.com>
2021-03-15 08:29:57 -0600

    segundo

---
+++ IS415ProjectOne/Models/Appointments.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace IS415ProjectOne.Models
+{
+    public class Appointments
+    {
+        //here we will put the code for the appointments
+    }
+}
---
+++ IS415ProjectOne/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IS415ProjectOne.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
